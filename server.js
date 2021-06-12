6ba9d0720f40019c35260ee22cb72a993066e54f
Surya Kumara <suryakumara33@gmail.com>
2021-06-12 12:15:28 +0800

    fix: setting routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// call routes
+const routes = require("./routes");
+routes(app);
+
 app.listen(3000, () => {
   console.log(`Server started on port`);
 });
